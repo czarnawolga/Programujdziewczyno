3aea549f2fea15843d51b28fb237281799c6c2fe
czarnawolga <32257217+czarnawolga@users.noreply.github.com>
2017-09-30 16:43:47 +0200

    zad. 2 pkt 8

--- js/app.js
+++ js/app.js
@@ -6,5 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
 	forDropdown.addEventListener('mouseout', function () {
 	menu.style.display = 'none';
 	});
-	
+	var buttons = document.querySelectorAll('.read-more');
+	console.log(buttons[0].previousElementSibling);
+	buttons[0].addEventListener('click', function () {
+      var textArea = this.previousElementSibling;
+      textArea.style.display = 'block'; });
+	console.log(this.innerHTML); 
+	buttons[0].addEventListener('click', function() {
+      var textArea = this.previousElementSibling;
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    });
+
 });
